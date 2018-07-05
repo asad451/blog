661fbc7d905ee4eed4235705d9cbb0c6e6624f3f
asad451 <asadmasood451@gmail.com>
2018-07-05 13:41:18 +0500

    Added Authentication using devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,18 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
-
-	http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+	before_action :authenticate_user!, except: [:index,:show]
+	#http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
 
   def index
   	@posts = Post.all.order("created_at DESC")
   end
 
   def new
-  	@post = Post.new
+  	@post = current_user.posts.build
   end
 
   def create
-  	@post = Post.new(post_params)
+  	@post = current_user.posts.build(post_params)
   	if @post.save
   		redirect_to @post 	#show page
   	else
